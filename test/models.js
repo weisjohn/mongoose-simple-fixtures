abed026fa4ee8977e27b7a423a425526130b320e
John Weis <weis.john@gmail.com>
2015-07-17 04:34:24 -0400

    adding sample models for people and site

---
+++ test/models.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+module.exports = function(cb) {
+
+    mongoose.model('people', (new mongoose.Schema({
+        first_name: String,
+        last_name: String,
+    })), 'people');
+
+    mongoose.model('site', (new mongoose.Schema({
+        url: String,
+        last_visit: {
+            type: Date,
+            default: Date.now
+        }
+    })));
+
+    cb();
+}
